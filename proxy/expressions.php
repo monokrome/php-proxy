d9440d11b01ee6a0e9387b291a9839c94f1293a2
Brandon R. Stoner <github@monokro.me>
2010-09-28 16:12:33 -0600

    Fixed the expressions file a bit.

--- proxy/expressions.php
+++ proxy/expressions.php
@@ -1,7 +1,9 @@
 <?php
-	// Add regular expressions that match your URLs to this list. Always use secure expressions, or you will be sad.
-	$expression_list = Array(
-	);
+	/**
+	 * URL rules / expressions.
+	 * TODO: Make it so that people can provide a custom method name to allow strings other than regex for comparing.
+	 */
 
+	// Add regular expressions that match your URLs to this Array. Always use secure expressions, or you will be sad.
 	return Array();
 ?>
