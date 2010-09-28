b0d4d154b6ee74def4197868805154fa2644fb85
Brandon R. Stoner <github@monokro.me>
2010-09-28 15:41:42 -0600

    A ton of small fixes/tweaks/improvements.

---
+++ proxy/headers.php
@@ -0,0 +1,8 @@
+<?php
+	/**
+	 * This should contain an array of headers that are to be set by the proxy on it's final response.
+	 */
+	return Array(
+		'Content-Type',
+	);
+?>
--- proxy/index.php
+++ proxy/index.php
@@ -6,7 +6,7 @@
 	 */
 
 	// Allows us to see all errors, notice, warnings, and obscurities
-	error_reporting(E_STRICT);
+	error_reporting(E_ALL | E_STRICT);
 
 	// Defines a bunch of errors that this file uses.
 	require_once('errors.php');
@@ -22,12 +22,14 @@
 		exit(HTTP_METHOD_ERROR);
 
 	// Gets the array of useful expressions from expressions.php
+	$transferable_headers = require('headers.php');
 	$url_expressions = require('expressions.php');
+
 	$url_accepted    = false; // Has our URL passed an expression test?
 
 	// Loop through our expressions searching for a match
 	for ($i=0; $i < count($url_expressions); ++$i)
-		if ($url_accepted == true || preg_match($url_expressions[$i], $_GET['destination']))
+		if (preg_match($url_expressions[$i], $_GET['destination']))
 		{
 			$url_accepted = true;
 			break;
@@ -63,8 +65,29 @@
 
 	$response = curl_exec($curl_descriptor);
 
-	if ($success === false)
+	if ($response === false)
 		exit(CURL_EXEC_FAILURE);
 
+	// Get the size of our header, so we know where to split the content from it.
+	$header_size = curl_getinfo($curl_descriptor, CURLINFO_HEADER_SIZE);
 	curl_close  ($curl_descriptor);
+
+	// Place the received headers into an array and remove the original HTTP header
+	$headers = explode("\n", substr($response, 0, $header_size));
+
+	// TODO: Make this a bit less insane/obstrusive
+	foreach ($headers as $header)
+	{
+		$matches = Array();
+
+		if (preg_match('/^([^:]+)\:\s*(.+)$/i', $header, $matches))
+		{
+			if (in_array($matches[1], $transferable_headers))
+			{
+				header($header);
+			}
+		}
+	}
+
+	print substr($response, $header_size, strlen($response)-$header_size);
 ?>
